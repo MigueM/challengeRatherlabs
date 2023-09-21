255feba23af5cc8396d44fc00234d5e9d04efb72
migue <miguem1994@gmail.com>
2023-09-21 16:44:56 -0300

    fix: 🐛 fix warnings in build

--- next.config.js
+++ next.config.js
@@ -1,23 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
 module.exports = {
-  ...nextConfig,
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    if (isServer) {
-      config.externals.push({
-        bufferutil: 'bufferutil',
-        'utf-8-validate': 'utf-8-validate',
-      })
-    }
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    })
     return config
   },
-}
-
-module.exports = {
   env: {
     REACT_APP_QUIZ_CONTRACT_ADDR: '0x437eF217203452317C3C955Cf282b1eE5F6aaF72',
     REACT_APP_ACCOUNT_ADD_PREFIX: '0x70a08231000000000000000000000000',
